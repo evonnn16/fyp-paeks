a097ff4fbdcc5502ab222a77183a431b677ba3d8
evon-kali <evonngyt1602@gmail.com>
2024-07-03 00:24:06 -0400

    Cleanup Directory

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Clone the Charm-Crypto repository
-git clone https://github.com/JHUISI/charm/ --depth 1 --branch=dev ~/charm
-cd ~/charm
-
-# Configure and install Charm-Crypto
-./configure.sh --python=python3.10 && make && sudo make install && sudo ldconfig
